0f0046b3a7f9b1ca4b78349be8c154e5759ed206
Hariharan <110547087+hariharan-cs21@users.noreply.github.com>
2023-09-15 21:11:53 +0530

    Update firebaseconfig.js

--- src/config/firebaseconfig.js
+++ src/config/firebaseconfig.js
@@ -4,12 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA1UX53R5_P4SsvtpMvntzJcc_G5QQ65ZM",
-    authDomain: "postwebsite-db060.firebaseapp.com",
-    projectId: "postwebsite-db060",
-    storageBucket: "postwebsite-db060.appspot.com",
-    messagingSenderId: "323811300326",
-    appId: "1:323811300326:web:d9dc652e6d7b5314473c27"
+   //api key hidden for security reasons
 };
 
 const app = initializeApp(firebaseConfig);
